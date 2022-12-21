522a4cf5a3f19f3737d1b58a2f4c89d3b917a1b0
TIMUR <otto@MacBook-Air-Timur.local>
2022-12-21 16:57:06 +0300

    Add new file src/locales/ru.js

---
+++ src/locales/ru.js
@@ -0,0 +1,19 @@
+export default {
+  translation: {
+    form: {
+      valid: 'RSS успешно загружен',
+      invalid: 'Ссылка должна быть валидным URL',
+    },
+    domain: {
+      errors: {
+        duplicate: 'RSS уже существует',
+        parseError: 'Ресурс не содержит валидный RSS',
+        networkError: 'Ошибка сети',
+        typeError: 'Неизвестная ошибка. Что-то пошло не так.',
+      },
+    },
+    buttons: {
+      view: 'Просмотр',
+    },
+  },
+};
